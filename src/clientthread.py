1dbd8d288cc1815fd85f5009e09e3ff3ba27192e
icedvariables <macodeblacko@gmail.com>
2015-04-02 17:42:47 +0100

    Added logging in

--- src/clientthread.py
+++ src/clientthread.py
@@ -8,31 +8,11 @@ def clientThread(c, addr):
 	c.send("<L>ogin to an existing character or <C>reate one: ")
 	option = c.recv(1024)
 	if(option.lower().strip() == "c"):
-		createCharacter(c)
+		player = createCharacter(c, addr)
+	else:
+		player = login(c, addr)
 
-	c.send("Username: ")
-        username = c.recv(1024).strip()
-	c.send("Password: ")
-        password = c.recv(1024).strip()
-	password = hashlib.sha224(password)
-	
-	print addr, "username:", username, "password:", password
-	
-	try:
-		player = Players.login(username, password)
-		thread.start_new_thread(clientthread.clientThread, (c, addr, player))
-	
-	except loginexceptions.IncorrectPassword as e:
-		print addr, "Incorrect password:", e
-		c.send("Incorrect password")
-		return
-	
-	except loginexceptions.UnknownPlayer as e:
-		print addr, "Unknown player:", e
-		c.send("Could not find a player with that name")
-                return
-
-def createCharacter(c):
+def createCharacter(c, addr):
 	c.send("Display name: ")
 	name = c.recv(1024)
 	c.send("Race: ")
@@ -48,3 +28,28 @@ def createCharacter(c):
 	player = Players.create(username, password, name, race)
 	Players.save()
 	return player
+
+def login(c, addr):
+	c.send("Username: ")
+        username = c.recv(1024).strip()
+	c.send("Password: ")
+        password = c.recv(1024).strip()
+	password = hashlib.sha224(password).hexdigest()
+	
+	print addr, "username:", username, "password:", password
+	
+	try:
+		player = Players.login(username, password)
+		print addr, "Correct password"
+		c.send("Correct password\n")
+	
+	except loginexceptions.IncorrectPassword as e:
+		print addr, "Incorrect password:", e
+		c.send("Incorrect password\n")
+		return
+	
+	except loginexceptions.UnknownPlayer as e:
+		print addr, "Unknown player:", e
+		c.send("Could not find a player with that name\n")
+                return
+
